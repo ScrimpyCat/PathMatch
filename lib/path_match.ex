a9ededc481846ab79e996ccc6b2795cbfddca472
Stefan Johnson <sj@scrimpycat.io>
2018-11-30 04:18:49 +1100

    Add docs

--- lib/path_match.ex
+++ lib/path_match.ex
@@ -56,10 +56,16 @@ defmodule PathMatch do
     defp path_match(path, [match|glob], precompiled, false), do: path_match(path, [match_to_regex(match)|glob], precompiled, true)
     defp path_match(_, _, _, _), do: false
 
+    @doc """
+      Match the given glob expression (compiled or uncompiled) against the path.
+    """
     @spec match?(String.t | [String.t | Regex.t], String.t) :: boolean
     def match?(glob, path) when is_list(glob), do: path_match(Path.split(path), glob, true)
     def match?(glob, path), do: path_match(Path.split(path), Path.split(glob))
 
+    @doc """
+      Pre-compile a glob expression for faster matching.
+    """
     @spec compile(String.t) :: [String.t | Regex.t]
     def compile(glob) do
         Path.split(glob)
